597d7f3e44513c8331c7ee401fa77c375e1c116e
jealou$y <wygonski@gmail.com>
2018-07-10 00:22:58 +0200

    added a currency converter

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const Discord = require('discord.js');
+const https = require('https');
 const client = new Discord.Client();
+
+const includes = require('./lib/includes');
 
 const config = require('./config');
 const aliases = require('./aliases');
+const currencies = require('./currencies');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+  // currency converter
+  let currency = includes(msg.content, currencies);
+
+  if(currency) {
+    let arg = msg.content.split(' ');
+    let amount = arg[currency - 1];
+    let convertTo = arg[currency + 1].substring(1).slice(0, -1);
+    let query = `${arg[currency]}_${convertTo}`;
+
+    console.log(`Converting ${amount} ${arg[currency]} to ${convertTo}`);
+
+    let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+
+    https.get(url, (res) => {
+      let body = '';
+
+      res.on('data', chunk => body += chunk);
+
+      res.on('end', () => {
+        try {
+          let jsonObj = JSON.parse(body);
+
+          if(jsonObj[query]) {
+            let converted = parseFloat(jsonObj[query] * amount).toFixed(2);
+
+            // update the original message
+            let _msg = '';
+            arg[currency + 1] = `(${converted} ${convertTo})`;
+
+            for(let i in arg) _msg += `${arg[i]} `;
+
+            msg.edit(_msg).catch(e => msg.channel.send(_msg));
+
+            console.log(`Successfully converted to ${converted} ${convertTo}`);
+          } else return console.log('An error occured: Can\'t find index for query');
+
+        } catch(e) {
+          console.log('An error occured: Can\'t process response');
+        }
+      });
+    }).on('error', (e) => console.log(`An error occured: ${e}`));
+  }
+
+  // avatar
   if(msg.content.startsWith(config.prefix + aliases['avatar'])) {
     let arg = msg.content.split(' ');
 
@@ -24,6 +72,7 @@ client.on('message', msg => {
     }
   }
 
+  // test (ping command)
   else if(msg.content === config.prefix + aliases['ping']) {
     msg.channel.send('pong');
   }
